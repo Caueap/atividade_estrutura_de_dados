8ee8049770edc2529d917bd3797652b94b24d164
Cauê <caueabproenca@gmail.com>
2022-10-22 17:21:48 -0300

    Primeiro commit

---
+++ src/atividade/Vetor.java
@@ -0,0 +1,34 @@
+package atividade;
+
+public class Vetor {
+
+
+    private Aluno[] alunos = new Aluno[10000];
+
+    private int totalDeAlunos = 0;
+
+    public void adiciona(Aluno aluno) {
+
+    }
+
+    public void adiciona(int posicao, Aluno aluno) {
+
+    }
+
+    public Aluno pega(int posicao) {
+        return null;
+    }
+
+    public void remove(int posicao) {
+
+    }
+
+    public boolean contem(Aluno aluno) {
+        return false;
+    }
+
+    public int tamanho() {
+        return 0;
+    }
+
+}
