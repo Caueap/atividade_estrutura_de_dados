f4195b8eb6aee08c5392ac4c7d0fd95899689e31
Cauê <caueabproenca@gmail.com>
2022-10-22 18:02:20 -0300

    Terceira questão

--- src/atividade/Vetor.java
+++ src/atividade/Vetor.java
@@ -31,4 +31,23 @@ public class Vetor {
         return 0;
     }
 
+    public String toString() {
+        if (this.totalDeAlunos == 0) {
+            return "[]";
+        }
+
+        StringBuilder builder = new StringBuilder();
+        builder.append("[");
+
+        for (int i = 0; i < this.totalDeAlunos - 1; i++) {
+            builder.append(this.alunos[i]);
+            builder.append(", ");
+        }
+
+        builder.append(this.alunos[this.totalDeAlunos - 1]);
+        builder.append("]");
+
+        return builder.toString();
+    }
+
 }
